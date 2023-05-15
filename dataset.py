e0e2fb62bba7b344c4e7b56499a1990d07563bfe
yudixie <yudixie235@gmail.com>
2023-05-15 16:14:37 -0400

    add hvm dataset and training

---
+++ dataset.py
@@ -0,0 +1,97 @@
+
+import os
+
+from skimage import io
+
+import numpy as np
+import pandas as pd
+from PIL import Image
+
+import torch
+from torch.utils.data import Dataset
+
+
+class HVMDataset(Dataset):
+    """hvm-public dataset."""
+    # my guess about the meaning of the collums
+    # s (size)
+    # tz, vertical translation
+    # ty, horizontal translation
+    # the rest seems to be rotations
+    # it seems that the following is true most of the time, but not always
+    # rxy_semantic - rxy = 90
+    # rxz_semantic = rxz
+    # ryz_semantic = ryz
+
+    # all collums in the stimulus set
+    all_collums = ['id', 'background_id', 's', 'image_id', 'image_file_name', 'filename', 'rxy', 'tz', 'category_name', 'rxz_semantic', 'ty', 'ryz', 'object_name', 'variation', 'size', 'rxy_semantic', 'ryz_semantic', 'rxz']
+    # collums that need to be predicted
+    pred_collums = ['category_name', 'object_name', 's', 'ty', 'tz', 'rxy', 'rxz', 'ryz']
+    # collums that need to be normalized
+    norm_collums = ['s', 'ty', 'tz', 'rxy', 'rxz', 'ryz']
+
+    def __init__(self, 
+                 csv_file='./data/image_dicarlo_hvm-public.csv', 
+                 root_dir='./data/image_dicarlo_hvm-public', 
+                 split='train',
+                 transform=None,
+                 ):
+        """
+        Arguments:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+        """
+        data_frame = pd.read_csv(csv_file)
+        normed_data_frame = data_frame.copy()
+        self.root_dir = root_dir
+        self.transform = transform
+      
+        # create a map from category name to category label
+        self.category_map = {}
+        cat_list = list(data_frame['category_name'].unique())
+        cat_list.sort()
+        for i, category_name in enumerate(cat_list):
+            self.category_map[category_name] = i
+        
+        # create a map from object name to object label
+        self.object_map = {}
+        object_list = list(data_frame['object_name'].unique())
+        object_list.sort()
+        for i, object_name in enumerate(object_list):
+            self.object_map[object_name] = i
+
+        # normalize the data
+        for collum in self.norm_collums:
+            normed_data_frame[collum] = (data_frame[collum] - data_frame[collum].mean()) / data_frame[collum].std()
+        
+        if split == 'train':
+            self.normed_data_frame = normed_data_frame[:int(len(normed_data_frame) * 0.8)]
+        elif split == 'val':
+            self.normed_data_frame = normed_data_frame[int(len(normed_data_frame) * 0.8):]
+        else:
+            raise ValueError('split must be either train or val')
+
+    def __len__(self):
+        return len(self.normed_data_frame)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_name = os.path.join(self.root_dir,
+                                self.normed_data_frame.loc[idx, 'image_file_name'])
+        image = Image.open(img_name)
+        if self.transform:
+            image = self.transform(image)
+        sample = {'image': image}
+        sample['category_label'] = self.category_map[self.normed_data_frame.loc[idx, 'category_name']]
+        sample['object_label'] = self.object_map[self.normed_data_frame.loc[idx, 'object_name']]
+
+        for collum in self.norm_collums:
+            sample[collum] = self.normed_data_frame.loc[idx, collum]
+
+        return sample
+
+
+if __name__ == '__main__':
+    pass
---
+++ main.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.models import resnet18, ResNet18_Weights
+
+from dataset import HVMDataset
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    model = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
+    # Replace the last layer with a linear layer for ImageNet classification
+    # num_objects = 8
+    # num_categories = 8
+    # num_predict = 6
+    # num_output = num_objects + num_categories + num_predict
+    model.fc = nn.Linear(model.fc.in_features, 8)
+    model = model.to(device)
+
+    # Data preprocessing
+    transform = transforms.Compose([
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+    ])
+
+    # Download and load the ImageNet dataset
+    train_dataset = HVMDataset(split='train', transform=transform)
+    val_dataset = HVMDataset(split='val', transform=transform)
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=4)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters())
+
+    # Train the model
+    model.train()
+    i_batch = 0
+    for data in train_loader:
+        inputs = data['image'].to(device)
+        labels = data['category_label'].to(device)
+
+        optimizer.zero_grad()
+
+        outputs = model(inputs)
+        _, preds = torch.max(outputs, 1)
+        loss = criterion(outputs, labels)
+
+        loss.backward()
+        optimizer.step()
+
+        print('Train loss: {}', loss.item())
