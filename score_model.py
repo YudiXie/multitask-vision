41fdd20725fac8bee6e19e470abb3e50dad6d235
YudiXie <yudixie235@gmail.com>
2023-05-27 17:49:28 -0400

    update score model

--- score_model.py
+++ score_model.py
@@ -4,7 +4,7 @@ import time
 
 import torch
 import torch.nn as nn
-from torchvision.models import resnet18
+from torchvision.models import resnet18, ResNet18_Weights
 
 from brainscore import score_model
 from model_tools.activations.pytorch import load_preprocess_images
@@ -34,17 +34,42 @@ resnet18layerlist = [
 
 
 benchmark_list = [
-    # 'movshon.FreemanZiemba2013public.V1-pls',
-    # 'movshon.FreemanZiemba2013public.V2-pls',
+    'movshon.FreemanZiemba2013public.V1-pls',
+    'movshon.FreemanZiemba2013public.V2-pls',
     'dicarlo.MajajHong2015public.V4-pls',
     'dicarlo.MajajHong2015public.IT-pls',
     'dicarlo.Rajalingham2018public-i2n',
     ]
 
+
 if __name__ == '__main__':
     # ImageNet pretrained model
     # model = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
 
+    # score pre-trained baseline model
+    model = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
+    model = model.to(DEVICE)
+    preprocessing = functools.partial(load_preprocess_images, image_size=224)
+    activations_model = PytorchWrapper(identifier='mt0527-resnet18-pret', model=model, preprocessing=preprocessing)
+    model = ModelCommitment(identifier='mt0527-resnet18-pret',
+                            activations_model=activations_model,
+                            layers=resnet18layerlist,
+                            behavioral_readout_layer='avgpool')
+    
+    for benchmark in benchmark_list:
+        # The score_model will score the model on the specified benchmark.
+        # When the model is asked to output activations for the IT region, it will first search for the best layer
+        # and then only output this layer's activations.
+        start_time = time.time()
+        score = score_model(model_identifier=model.identifier, model=model,
+                            benchmark_identifier=benchmark)
+        print(score)
+        center, error = score.sel(aggregation='center'), score.sel(aggregation='error')
+        print(f"Score: {center.values:.3f}+-{error.values:.3f}")
+        print("Run time %.2f mins" % ((time.time() - start_time) / 60))
+
+
+    # score experiments models
     for run_id in range(10):
         save_path = os.path.join(EXP_DIR, f'multi_task_vs_categorization0527/run_000{run_id}', 'model.pth')
 
@@ -54,9 +79,9 @@ if __name__ == '__main__':
         model.load_state_dict(torch.load(save_path, map_location=DEVICE), strict=True)
 
         preprocessing = functools.partial(load_preprocess_images, image_size=224)
-        activations_model = PytorchWrapper(identifier=f'mt-resnet18-{run_id}', model=model, preprocessing=preprocessing)
+        activations_model = PytorchWrapper(identifier=f'mt0527-resnet18-{run_id}', model=model, preprocessing=preprocessing)
 
-        model = ModelCommitment(identifier=f'mt-resnet18-{run_id}',
+        model = ModelCommitment(identifier=f'mt0527-resnet18-{run_id}',
                                 activations_model=activations_model,
                                 layers=resnet18layerlist,
                                 behavioral_readout_layer='avgpool')
