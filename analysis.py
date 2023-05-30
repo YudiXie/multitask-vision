d6a8530cf270f41af5b7e92cfe2bf1b43b842046
YudiXie <yudixie235@gmail.com>
2023-05-29 21:31:11 -0400

    figure to analyze significance

--- analysis.py
+++ analysis.py
@@ -1,4 +1,5 @@
 import os
+import scipy.stats
 
 import numpy as np
 import pandas as pd
@@ -17,6 +18,152 @@ def adjust_figure(ax=None):
     # ax.yaxis.set_ticks_position('left')
     # ax.xaxis.set_ticks_position('bottom')
     plt.tight_layout(pad=0.5)
+
+
+def get_stat_str(p_value, maxasterix=None):
+    """
+    Get asterix string based on p value
+    :param p_value:
+    :param maxasterix: maximum number of asterixes to write (for very small p-values)
+    :return:
+    """
+    # * is p < 0.05
+    # ** is p < 0.005
+    # *** is p < 0.0005
+    # etc.
+    text = ''
+    p = .05
+
+    while p_value < p:
+        text += '*'
+        p /= 10.
+
+        if maxasterix and len(text) == maxasterix:
+            break
+
+    if len(text) == 0:
+        text = 'n. s.'
+
+    return text
+
+
+def barplot_annotate_brackets(idx1, idx2, data, center, height, yerr=None, dh=.05, barh=.05, fs=None):
+    """
+    Annotate barplot with p-values.
+    adapted from:
+    https://stackoverflow.com/questions/11517986/indicating-the-statistically-significant-difference-in-bar-graph
+
+    :param idx1: index of left bar to put bracket over
+    :param idx2: index of right bar to put bracket over
+    :param data: string to write or number for generating asterixes
+    :param center: centers of all bars (like plt.bar() input)
+    :param height: heights of all bars (like plt.bar() input)
+    :param yerr: yerrs of all bars (like plt.bar() input)
+    :param dh: height offset over bar / bar + yerr in axes coordinates (0 to 1)
+    :param barh: bar height in axes coordinates (0 to 1)
+    :param fs: font size
+    """
+
+    if type(data) is str:
+        text = data
+    else:
+        text = get_stat_str(data)
+
+    lx, ly = center[idx1], height[idx1]
+    rx, ry = center[idx2], height[idx2]
+
+    if yerr:
+        ly += yerr[idx1]
+        ry += yerr[idx2]
+
+    ax_y0, ax_y1 = plt.gca().get_ylim()
+    dh *= (ax_y1 - ax_y0)
+    barh *= (ax_y1 - ax_y0)
+
+    y = max(ly, ry) + dh
+
+    barx = [lx, lx, rx, rx]
+    bary = [y, y+barh, y+barh, y]
+    mid = ((lx+rx)/2, y+barh)
+
+    plt.plot(barx, bary, c='black')
+
+    kwargs = dict(ha='center', va='bottom')
+    if fs is not None:
+        kwargs['fontsize'] = fs
+
+    plt.text(*mid, text, **kwargs)
+
+
+def two_set_scatter_plot(data1, data2, labels, title_str, ylabel, save_str):
+    """
+    scatter plot for two groups
+        :param data1: pandas series for data group 1
+        :param data2: pandas series for data group 1
+        :param labels: labels for two groups
+        :param title_str: string for title
+        :param ylabel: y label string
+        :param save_str: string to save
+    """
+    mean_data1 = data1.mean()
+    sem_data1 = data1.sem()
+    data1_x = [1, ] * len(data1)
+
+    mean_data2 = data2.mean()
+    sem_data2 = data2.sem()
+    data2_x = [2, ] * len(data2)
+
+    # Mann-Whitney U test, or Wilcoxon rank-sum test
+    p_value = scipy.stats.mannwhitneyu(data1.dropna(), data2.dropna()).pvalue
+    wilcoxon_p_value = scipy.stats.ranksums(data1.dropna(), data2.dropna()).pvalue
+
+    # Shapiro-Wilk test of normality
+    l_normal_p = scipy.stats.shapiro(data1.dropna()).pvalue
+    r_normal_p = scipy.stats.shapiro(data2.dropna()).pvalue
+
+    # T-test
+    ttest_p_value = scipy.stats.ttest_ind(data1.dropna(), data2.dropna()).pvalue
+
+    plt.figure(figsize=(2.5, 5))
+    plt.scatter(data1_x, data1, 100, color='w', edgecolors='k', alpha=0.5)
+    plt.errorbar([1], mean_data1, yerr=sem_data1,
+                 fmt="o",
+                 mfc='white',
+                 ecolor='k',
+                 color='k',
+                 elinewidth=1,
+                 capsize=10,
+                 markersize=10
+                 )
+
+    plt.scatter(data2_x, data2, 100, color='mediumorchid', edgecolors='k', alpha=0.5)
+    plt.errorbar([2], mean_data2, yerr=sem_data2,
+                 fmt="o",
+                 mfc='mediumorchid',
+                 ecolor='k',
+                 color='k',
+                 elinewidth=1,
+                 capsize=10,
+                 markersize=10
+                 )
+
+    plt.xticks([1, 2], labels, rotation=45)
+    plt.locator_params(nbins=5, axis='y')
+    plt.xlim([0.5, 2.5])
+    plt.title(title_str
+              + "\nMann-Whitney U test P = {:.3f}".format(p_value)
+              + "\nWilcoxon rank-sum test P = {:.3f}".format(wilcoxon_p_value)
+              + "\nNormality test (left, right) P = ({:.3f}, {:.3f})".format(l_normal_p, r_normal_p)
+              + "\nT-test P = {:.3f}".format(ttest_p_value),
+              fontsize=12)
+    plt.ylabel(ylabel)
+    barplot_annotate_brackets(0, 1, p_value, [1, 2],
+                              [mean_data1, mean_data2],
+                              [data1.max()-mean_data1, data2.max()-mean_data1],
+                              barh=0.025)
+    adjust_figure()
+    plt.savefig(os.path.join('./figures/', save_str + '.pdf'), transparent=True, bbox_inches="tight")
+    plt.close()
 
 
 def bar_2par(data,
@@ -118,3 +265,12 @@ if __name__ == '__main__':
             fig_name='compare_different_groups',
             y_label='Score',)
     
+    for i, (region, benchmark) in enumerate(zip(x_axis_labels, benchmark_list)):
+        # each dot in the plot is an animal per sessions
+        cat_data = df[(df['exp_group'] == 'Categorization') & (df['benchmark'] == benchmark)]['score']
+        mul_data = df[(df['exp_group'] == 'Multi-task') & (df['benchmark'] == benchmark)]['score']
+        two_set_scatter_plot(cat_data, mul_data,
+                            labels=['Categorization', 'Multi-task'],
+                            title_str=f'{region} Score',
+                            ylabel='Score',
+                            save_str=f'fig{i}_{region}_score_dot_random_seed')
