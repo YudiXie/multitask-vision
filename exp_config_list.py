5913190730952d1f58c94865e53206ad86bfa20c
YudiXie <yudixie235@gmail.com>
2023-08-20 16:00:17 -0400

    add tdw category training

--- exp_config_list.py
+++ exp_config_list.py
@@ -200,3 +200,37 @@ def multi_task_tdw_0817():
             config_list.append(cfg)
             run_id += 1
     return config_list
+
+
+def cat_diff_tdw_0820():
+    # compare models trained with categorization tasks with different number of output units
+    exp_config = copy.deepcopy(base_config)
+    exp_config['experiment_name'] = 'cat_diff_tdw_0820'
+    exp_config['dataset_name'] = 'TDW'
+
+    task_set_dict = {
+        'cat2': ['cat2'],
+        'cat3': ['cat3'],
+        'cat4': ['cat4'],
+        'cat5': ['cat5'],
+        'cat6': ['cat6'],
+        'cat7': ['cat7'],
+        'cat8': ['cat8'],
+    }
+    seed_list = [0, 1, 2, 3, 4]
+    
+    # setting up config list
+    config_list = []
+    run_id = 0
+    for group_n, task_set in task_set_dict.items():
+        for seed in seed_list:
+            cfg = copy.deepcopy(exp_config)
+            cfg['group_name'] = group_n
+            cfg['tasks'] = task_set
+            cfg['seed'] = seed
+
+            cfg['save_path'] = os.path.join(EXP_DIR, cfg['experiment_name'], f'run_{run_id:04d}')
+            cfg['run_id'] = run_id
+            config_list.append(cfg)
+            run_id += 1
+    return config_list
