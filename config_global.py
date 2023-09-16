c719d608b92432f645d019c0d772edd94660409e
YudiXie <yudixie235@gmail.com>
2023-09-16 11:52:31 -0400

    update device

--- config_global.py
+++ config_global.py
@@ -11,7 +11,7 @@ DATA_DIR = osp.join(ROOT_DIR, 'data')
 
 # device to run algorithm on
 USE_CUDA = torch.cuda.is_available()
-DEVICE = torch.device("cuda" if USE_CUDA else "cpu")
+DEVICE = torch.device('cuda:0' if USE_CUDA else 'cpu')
 
 CUDA_MODULE = 'openmind8/cuda/11.7'
 CONDA_ENV = 'multitask-vision'
