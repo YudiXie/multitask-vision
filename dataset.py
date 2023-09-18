30ba3c432a0120f0deeaac9714d1f1ae387ed21c
yudixie <yudixie235@gmail.com>
2023-09-18 11:54:04 -0400

    add dataset preprocessing

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 
 import os
 from pathlib import Path
+import argparse
 
 import numpy as np
 import pandas as pd
@@ -10,6 +11,7 @@ import torch
 from torch.utils.data import Dataset
 from skimage import io
 from tqdm import tqdm
+import yaml
 
 
 def center_circ_array(arr):
@@ -148,56 +150,24 @@ class TDWDataset(Dataset):
     norm_collums = ['neg_x', 'ty', 'tz','euler_1_proc', 'euler_2_proc', 'euler_3_proc']
 
     def __init__(self, 
-                 csv_file='./data/tdw_image_dataset_small_multi_env_hdri/images_meta_shuffled.csv',
                  root_dir='./data/tdw_image_dataset_small_multi_env_hdri',
                  split='train',
                  transform=None,
                  ):
         """
         Arguments:
-            csv_file (string): Path to the csv file with annotations.
             root_dir (string): Directory with all the images.
         """
-        data_frame = pd.read_csv(csv_file, index_col=0)
-        # shuffle the data
-        # data_frame = data_frame.sample(frac=1, random_state=RNG).reset_index(drop=True)
+        assert Path(root_dir).joinpath('images_meta_shuffled_processed.csv').is_file(), \
+            f'images_meta_shuffled_processed.csv does not exist in {root_dir}, please run tdw_dataset_preprocess() first'
+        normed_data_frame = pd.read_csv(os.path.join(root_dir, 'images_meta_shuffled_processed.csv'), index_col=0)
 
-        # check if all images exist
-        data_path = Path(root_dir)
-        for i in tqdm(range(len(data_frame))):
-            assert data_path.joinpath(data_frame.loc[i, 'image_filename']).is_file()
-
-        normed_data_frame = data_frame.copy()
         self.root_dir = root_dir
         self.transform = transform
 
-        # process the euler angles
-        data_frame['euler_1_proc'] = center_circ_array(data_frame['euler_1'].to_numpy())
-        data_frame['euler_2_proc'] = center_circ_array(data_frame['euler_2'].to_numpy())
-        data_frame['euler_3_proc'] = center_circ_array(data_frame['euler_3'].to_numpy())
-      
-        # create a map from category name to category label
-        self.category_str2int = {}
-        cat_list = list(normed_data_frame['wnid'].unique())
-        cat_list.sort()
-        for i, category_name in enumerate(cat_list):
-            self.category_str2int[category_name] = i
-        self.category_int2str = {v: k for k, v in self.category_str2int.items()}
-        normed_data_frame['category_label'] = [self.category_str2int[cn] for cn in normed_data_frame['wnid']]
-        
-        # create a map from object name to object label
-        self.object_str2int = {}
-        object_list = list(normed_data_frame['record_name'].unique())
-        object_list.sort()
-        for i, object_name in enumerate(object_list):
-            self.object_str2int[object_name] = i
-        self.object_int2str = {v: k for k, v in self.object_str2int.items()}
-        normed_data_frame['object_label'] = [self.object_str2int[on] for on in normed_data_frame['record_name']]
-
-        # normalize the data
-        for collum in self.norm_collums:
-            normed_data_frame[collum] = (data_frame[collum] - data_frame[collum].mean()) / data_frame[collum].std()
-            normed_data_frame[collum] = normed_data_frame[collum].astype(np.float32)
+        with open(os.path.join(root_dir, 'mappings.yml'), 'r') as file:
+            mappings = yaml.safe_load(file)
+        self.mapping = mappings
         
         if split == 'all':
             self.normed_data_frame = normed_data_frame
@@ -233,5 +203,73 @@ class TDWDataset(Dataset):
         return sample
 
 
+def create_mapping(str_list: list):
+    """
+    Create a mapping from string to int and int to string
+    param: str_list: list of strings with no duplicates
+    return: two dictionaries that contains the mapping from string to int and int to string
+    """
+    str_list.sort()
+    str2int_map = {}
+    for i, str_name in enumerate(str_list):
+        str2int_map[str_name] = i
+    int2str_map = {v: k for k, v in str2int_map.items()}
+    return str2int_map, int2str_map
+
+
+def tdw_dataset_preprocess(root_dir):
+    """
+    Preprocess the TDW dataset.
+    """
+    csv_path = os.path.join(root_dir, 'images_meta_shuffled.csv')
+    data_frame = pd.read_csv(csv_path, index_col=0)
+    # shuffle the data
+    # data_frame = data_frame.sample(frac=1, random_state=RNG).reset_index(drop=True)
+
+    # check if all images exist
+    data_path = Path(root_dir)
+    for i in tqdm(range(len(data_frame))):
+        assert data_path.joinpath(data_frame.loc[i, 'image_filename']).is_file()
+    print(f'All images in csv file {csv_path} exist')
+
+    normed_data_frame = data_frame.copy()
+
+    # create a map from category name to category label
+    category_str2int, category_int2str = create_mapping(list(normed_data_frame['wnid'].unique()))
+    normed_data_frame['category_label'] = [category_str2int[cn] for cn in normed_data_frame['wnid']]
+    
+    # create a map from object name to object label
+    object_str2int, object_int2str = create_mapping(list(normed_data_frame['record_name'].unique()))
+    normed_data_frame['object_label'] = [object_str2int[on] for on in normed_data_frame['record_name']]
+
+    mappings = {
+        'category_str2int': category_str2int,
+        'category_int2str': category_int2str,
+        'object_str2int': object_str2int,
+        'object_int2str': object_int2str,
+    }
+
+    # process the euler angles, center them around 0, and in range (-180, 180)
+    data_frame['euler_1_proc'] = center_circ_array(data_frame['euler_1'].to_numpy())
+    data_frame['euler_2_proc'] = center_circ_array(data_frame['euler_2'].to_numpy())
+    data_frame['euler_3_proc'] = center_circ_array(data_frame['euler_3'].to_numpy())
+
+    # normalize the data
+    for collum in TDWDataset.norm_collums:
+        normed_data_frame[collum] = (data_frame[collum] - data_frame[collum].mean()) / data_frame[collum].std()
+        normed_data_frame[collum] = normed_data_frame[collum].astype(np.float32)
+    
+    # save the processed data
+    normed_data_frame.to_csv(str(data_path.joinpath('images_meta_shuffled_processed.csv').resolve()))
+    yaml_file_path = os.path.join(root_dir, 'mappings.yml')
+    with open(yaml_file_path, 'w') as file:
+        yaml.dump(mappings, file, default_flow_style=False)
+
+
 if __name__ == '__main__':
-    pass
+    # run this script to save model scores for all models in an experiment
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--name', help='Name of the dataset')
+    args = parser.parse_args()
+    
+    tdw_dataset_preprocess(root_dir = os.path.join('./data/', args.name))
