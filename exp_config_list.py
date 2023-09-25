ace459e11a9fcd1e6a1c49771794f80bcb022cda
yudixie <yudixie235@gmail.com>
2023-09-25 16:38:18 -0400

    add experiments without pre-training

--- exp_config_list.py
+++ exp_config_list.py
@@ -292,3 +292,47 @@ def multi_task_tdw_large20230907_0919():
         
         new_config_list.append(config)
     return new_config_list
+
+
+def multi_task_tdw_large20230907_nopret_0925():
+    # compare models with different training targets
+    exp_config = copy.deepcopy(base_config)
+    exp_config['experiment_name'] = 'multi_task_tdw_large20230907_nopret_0925'
+    exp_config['dataset_name'] = 'TDW_large20230907'
+    exp_config['max_batch'] = 200000  # run thorugh the dataset ~10 times with batchsize 64
+    exp_config['eval_per'] = 1000
+    exp_config['checkpoint_per'] = 1000
+    exp_config['pretrain_init'] = False
+
+    task_set_dict = {
+        'distance_reg': ['distance_reg'],
+        'translation_reg': ['translation_reg'],
+        'rotation_reg': ['rotation_reg_tdw'],
+
+        'distance_translation': ['distance_reg', 'translation_reg'],
+        'distance_rotation': ['distance_reg', 'rotation_reg_tdw'],
+        'translation_rotation': ['translation_reg', 'rotation_reg_tdw'],
+
+        'distance_translation_rotation': ['distance_reg', 'translation_reg', 'rotation_reg_tdw'],
+
+        'category_class': ['category_class'],
+        'object_class': ['object_class'],
+        'cat_obj_class_all_latents': ['category_class', 'object_class', 'rotation_reg_tdw', 'distance_reg', 'translation_reg'],
+    }
+    seed_list = [0, 1, 2]
+    
+    # setting up config list
+    config_list = []
+    run_id = 0
+    for group_n, task_set in task_set_dict.items():
+        for seed in seed_list:
+            cfg = copy.deepcopy(exp_config)
+            cfg['group_name'] = group_n
+            cfg['tasks'] = task_set
+            cfg['seed'] = seed
+
+            cfg['save_path'] = os.path.join(EXP_DIR, cfg['experiment_name'], f'run_{run_id:04d}')
+            cfg['run_id'] = run_id
+            config_list.append(cfg)
+            run_id += 1
+    return config_list
