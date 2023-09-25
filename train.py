5a47f09dfd3756f69c6fb4c7d90050bd3597d59a
yudixie <yudixie235@gmail.com>
2023-09-25 17:46:48 -0400

    minor

--- train.py
+++ train.py
@@ -16,6 +16,7 @@ from dataset import HVMDataset, TDWDataset
 from utils import load_config, log_complete
 from tasks_setup import cat_reduced_tasks, task2loss_func, task2targets_name, get_output_info
 
+
 # used to set the weights of the different tasks by hand
 # note that when less than 5 tasks are used, the weights should be adjusted
 # so that they sum to 1
