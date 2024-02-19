b5e39cd6356de63e5cb1af4cb741a0303d009231
YudiXie <yudixie235@gmail.com>
2024-02-19 15:01:02 -0500

    update preprocessing to include index_img_shuffled_with_meta

--- tdw_dataset_preprocess.py
+++ tdw_dataset_preprocess.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import argparse
+from collections import defaultdict
 
 import numpy as np
 from tqdm import trange
@@ -29,9 +30,25 @@ if __name__ == '__main__':
     assert dataset_path.joinpath("dataset_complete.txt").is_file(), "No complete check file found"
 
     index_df = pd.read_csv(index_path, names=['image_index', 'scene', 'wnid', 'model'], skiprows=1)
+    meta_headers = dataset_path.joinpath('img_meta_headers.txt').read_text(encoding="utf-8").split("\n")
     # shuffle the index, and save a copy on disk
     shuffled_index = index_df.sample(frac=1).reset_index(drop=True)
     shuffled_index.to_csv(dataset_path.joinpath("index_img_shuffled.csv"))
+
+    # concatenate the meta data to the index and save a copy on disk
+    # read CSV files string convertion, reverse indexing to avoid cases like "bag, handbag, pocketbook, purse
+    inv_idx = {header: - (len(meta_headers) - i) for i, header in enumerate(meta_headers)}
+    append_data = defaultdict(list)
+    for i_row in trange(len(shuffled_index)):
+        _, img_meta_path = get_image_meta_path(shuffled_index, i_row, dataset_path)
+        data_list = img_meta_path.read_text().split(",")
+        for column in TDWDataset.vis_collumns:
+            append_data[column].append(float(data_list[inv_idx[column]]))
+    
+    for k, v in append_data.items():
+        shuffled_index[k] = np.array(v, dtype=np.float32)
+    
+    shuffled_index.to_csv(dataset_path.joinpath('index_img_shuffled_with_meta.csv'))
 
     # --------------------------------
     # create a map from category name to category label
@@ -53,7 +70,6 @@ if __name__ == '__main__':
     # sample some data to calculate the mean and std of columns in norm_columns
     sample_size = min(100000, len(shuffled_index))
     sample_index = shuffled_index.iloc[:sample_size]
-    meta_headers = dataset_path.joinpath('img_meta_headers.txt').read_text(encoding="utf-8").split("\n")
     
     img_meta_rows = []
     for i_row in trange(len(sample_index)):
