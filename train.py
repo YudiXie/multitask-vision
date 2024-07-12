d4dcb2169d5df9a2f9fd809e12a0b1f5ac243da5
YudiXie <yudixie235@gmail.com>
2024-07-12 11:00:17 -0400

    add 1c data

--- train.py
+++ train.py
@@ -75,6 +75,11 @@ def get_dataloader(dataset_name, is_train, batch_size, transform, dataset_fracti
                              split=split,
                              transform=transform,
                              fraction=dataset_fraction,)
+    elif dataset_name == 'tdw_1m_1c_n03001627_20240711':
+        dataset = TDWDataset(root_dir='/om/user/yu_xie/data/tdw_images/tdw_image_dataset_1m_1c_n03001627_20240711', 
+                             split=split,
+                             transform=transform,
+                             fraction=dataset_fraction,)
     else:
         raise NotImplementedError(f'Unknown dataset: {dataset_name}')
     
