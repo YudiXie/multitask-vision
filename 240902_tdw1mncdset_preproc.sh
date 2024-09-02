9fc8f7c49c8cb161a8040749c12ab9a1f0aa987f
YudiXie <yudixie235@gmail.com>
2024-09-02 14:35:39 -0400

    update

---
+++ 240902_tdw1mncdset_preproc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -t 8:00:00
+#SBATCH -N 1
+#SBATCH -n 12
+#SBATCH --mem=64G
+#SBATCH --gres=gpu:a100:1
+#SBATCH --partition=normal
+#SBATCH -e /om/weka/dicarlo/yu_xie/projects/multitask-vision/slurm_output/slurm-%j-preproc.out
+#SBATCH -o /om/weka/dicarlo/yu_xie/projects/multitask-vision/slurm_output/slurm-%j-preproc.out
+
+source ~/.bashrc
+echo -e "System Info: \n----------\n$(hostnamectl)\n----------"
+cd /om/user/yu_xie/projects/multitask-vision
+conda activate mtvision
+python tdw_dataset_preprocess.py --index $1
+echo "preprocess dataset $1 finished!"
