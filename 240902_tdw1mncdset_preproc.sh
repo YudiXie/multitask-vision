b1df7431bca2f3d8b588ac2c4370749335bc264d
YudiXie <yudixie235@gmail.com>
2024-09-02 14:36:46 -0400

    update

--- 240902_tdw1mncdset_preproc.sh
+++ 240902_tdw1mncdset_preproc.sh
@@ -11,6 +11,6 @@
 source ~/.bashrc
 echo -e "System Info: \n----------\n$(hostnamectl)\n----------"
 cd /om/user/yu_xie/projects/multitask-vision
-conda activate mtvision
+conda activate mtvision3
 python tdw_dataset_preprocess.py --index $1
 echo "preprocess dataset $1 finished!"
