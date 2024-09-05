6dec25ebb1d4be10ba9531579bbce5a4d9deee14
YudiXie <yudixie235@gmail.com>
2024-09-05 17:47:49 -0400

    update

--- analysis_scripts/240905_resnet18_bscore.py
+++ analysis_scripts/240905_resnet18_bscore.py
@@ -115,7 +115,7 @@ bp.add_errorbars(ax, data_dict, fmt="o")
 ax.hlines(rnd_data, 1, 1000, linestyles='dashed', label='Untrained', color='k')
 ax.fill_between([1, 1000], 2 * [rnd_data - rnd_error], 2 * [rnd_data + rnd_error], alpha=0.2, color='k')
 
-ax.set_xlabel('Number of CNN output units')
+ax.set_xlabel('Number of supervised output units')
 ax.set_ylabel('Mean alignment score \n (V1, V2, V4, IT)')
 ax.set_yticks([0.25, 0.30, 0.35, 0.40], ['0.25', '', '', '0.40'])
 ax.set_xscale('log')
@@ -123,5 +123,92 @@ ax.legend(loc=(0.35, 0.17), fontsize=9)
 bp.remove_top_right_spines(ax)
 fig.tight_layout()
 fig.savefig(Path(FIG_DIR).joinpath(f'brainscore_vs_output_num_resnset18_meanV1V2V4IT.pdf'), transparent=True)
+
+# %%
+for region in ['V1', 'V2', 'V4', 'IT', 'Behavior']:
+    df_mt_agg = df_mt[df_mt['benchmark_region'] == region].groupby('exp_group')['score'].agg(['mean', 'std'])
+
+    latent_data = list(df_mt_agg['mean'].reindex(latent_task_list))
+    latent_error = list(df_mt_agg['std'].reindex(latent_task_list))
+
+    cat_class_data = [df_mt_agg['mean']['category_class'], ]
+    cat_class_error = [df_mt_agg['std']['category_class'], ]
+
+    obj_class_data = [df_mt_agg['mean']['object_class'], ]
+    obj_class_error = [df_mt_agg['std']['object_class'], ]
+
+    mlt_data = [df_mt_agg['mean']['cat_obj_class_all_latents'], ]
+    mlt_error = [df_mt_agg['std']['cat_obj_class_all_latents'], ]
+
+    # random untrained models and imagenet pre-trained models
+    df_pt_rnd_agg = df_pt_rnd[df_pt_rnd['benchmark_region'] == region].groupby('exp_group')['score'].agg(['mean', 'std'])
+    rnd_data = df_pt_rnd_agg['mean']['random']
+    rnd_error = df_pt_rnd_agg['std']['random']
+
+    # pytorch imagenet models, all the scores for the pre-trained models are the same, so the error is 0
+    pt_data = df_pt_rnd_agg['mean']['imagenet1k_pretrain']
+    pt_error = df_pt_rnd_agg['std']['imagenet1k_pretrain']
+
+    # cateogry classification on datasets with different number of categories
+    df_nc_cat_agg = df_nc_cat[df_nc_cat['benchmark_region'] == region].groupby('exp_group')['score'].agg(['mean', 'std'])
+    nc_data = list(df_nc_cat_agg['mean'].reindex(nc_list))
+    nc_error = list(df_nc_cat_agg['std'].reindex(nc_list))
+
+    # in hous imagenet classification models
+    df_imn_neural_s = df_imn[df_imn['benchmark_region'] == region]['score']
+    imn_data, imn_error = df_imn_neural_s.mean(), df_imn_neural_s.std()
+
+
+    data_dict = {
+        'Latent variable reg. (TDW-117)': {
+            'x': latent_output_num_list,
+            'y': latent_data,
+            'error': latent_error,
+        },
+        'Object category cla. (TDW-117)': {
+            'x': [117, ],
+            'y': cat_class_data,
+            'error': cat_class_error,
+        },
+        'Object category cla. (TDW-N)': {
+            'x': nc_num_list,
+            'y': nc_data,
+            'error': nc_error,
+            'kwargs': {'color': '#FFAA5A'},
+        },
+        'Object identity cla. (TDW-117)': {
+            'x': [548, ],
+            'y': obj_class_data,
+            'error': obj_class_error,
+        },
+        'All cla. + all reg. (TDW-117)': {
+            'x': [674, ],
+            'y': mlt_data,
+            'error': mlt_error,
+        },
+        'Object category cla. (ImageNet-1K)': {
+            'x': [900, ],
+            'y': [imn_data, ],
+            'error': [imn_error, ],
+            'kwargs': {'color': 'r'},
+        },
+    }
+
+    fig, ax = plt.subplots(figsize=(4.8, 3.6))
+    bp.add_errorbars(ax, data_dict, fmt="o")
+    # ax.scatter([1000, ], [pt_data, ], label='ImageNet-1K', color='r', marker='D')
+    ax.hlines(rnd_data, 1, 1000, linestyles='dashed', label='Untrained', color='k')
+    ax.fill_between([1, 1000], 2 * [rnd_data - rnd_error], 2 * [rnd_data + rnd_error], alpha=0.2, color='k')
+
+    ax.set_xlabel('Number of supervised output units')
+    ax.set_ylabel(f'{region} alignment score')
+    # ax.set_yticks([0.25, 0.30, 0.35, 0.40], ['0.25', '', '', '0.40'])
+    ax.set_xscale('log')
+    bp.remove_top_right_spines(ax)
+    fig.tight_layout()
+    fig.savefig(Path(FIG_DIR).joinpath(f'brainscore_vs_output_num_resnset18_mean{region}.pdf'), transparent=True)
+
+# %%
+
 
 
