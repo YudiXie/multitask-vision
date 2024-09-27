52a458b5316b2cebd8df6eac8ffe15a3dff01bc3
YudiXie <yudixie235@gmail.com>
2024-09-27 18:13:10 -0400

    add new ctrl var experiments

--- exp_config_list.py
+++ exp_config_list.py
@@ -1018,3 +1018,80 @@ def imagenet1k_resnet50_0908():
         config_list.append(cfg)
         run_id += 1
     return config_list
+
+
+def ctrl_cat_var_240927():
+    exp_config = copy.deepcopy(base_config)
+    exp_config['experiment_name'] = 'ctrl_cat_var_240927'
+
+    exp_config['max_batch'] = 500000  # run thorugh the dataset ~30 times with batchsize 64
+    exp_config['eval_per'] = 10000
+    exp_config['checkpoint_per'] = 1000
+    exp_config['pretrain_init'] = False
+    
+    dset_list = ['tdw_1m_20240206', 
+                 'tdw_1m_1c_n03001627_20240711']
+    task_set_dict = {
+        'distance_reg': ['distance_reg'],
+        'translation_reg': ['translation_reg'],
+        'rotation_reg': ['rotation_reg_tdw_two_units_sin_cos_mse'],
+        'distance_translation_rotation': ['distance_reg', 'translation_reg', 'rotation_reg_tdw_two_units_sin_cos_mse'],
+    }
+    seed_list = [0, 1, 2, 3, 4, 5]
+
+    # setting up config list
+    config_list = []
+    run_id = 0
+    for dset in dset_list:
+        for task_set_n, task_set in task_set_dict.items():
+            for seed in seed_list:
+                cfg = copy.deepcopy(exp_config)
+                cfg['dataset_name'] = dset
+                cfg['task_set_name'] = task_set_n
+                cfg['tasks'] = task_set
+                cfg['seed'] = seed
+
+                cfg['save_path'] = os.path.join(EXP_DIR, cfg['experiment_name'], f'run_{run_id:04d}')
+                cfg['run_id'] = run_id
+                config_list.append(cfg)
+                run_id += 1
+    return config_list
+
+
+# compare with a dataset that has little variations in translation
+def ctrl_trans_var_240927():
+    exp_config = copy.deepcopy(base_config)
+    exp_config['experiment_name'] = 'ctrl_trans_var_240927'
+
+    exp_config['max_batch'] = 500000  # run thorugh the dataset ~30 times with batchsize 64
+    exp_config['eval_per'] = 10000
+    exp_config['checkpoint_per'] = 1000
+    exp_config['pretrain_init'] = False
+    
+    dset_list = ['tdw_1m_20240206', 
+                 'tdw_1m_obj_centered_20240812']
+    task_set_dict = {
+        'distance_reg': ['distance_reg'],
+        'rotation_reg': ['rotation_reg_tdw_two_units_sin_cos_mse'], # rotation is affected in tdw_1m_obj_centered_20240812 but still adding it here
+        'distance_rotation': ['distance_reg', 'rotation_reg_tdw_two_units_sin_cos_mse'],
+        'category_class': ['category_class'],
+    }
+    seed_list = [0, 1, 2, 3, 4, 5]
+
+    # setting up config list
+    config_list = []
+    run_id = 0
+    for dset in dset_list:
+        for task_set_n, task_set in task_set_dict.items():
+            for seed in seed_list:
+                cfg = copy.deepcopy(exp_config)
+                cfg['dataset_name'] = dset
+                cfg['task_set_name'] = task_set_n
+                cfg['tasks'] = task_set
+                cfg['seed'] = seed
+
+                cfg['save_path'] = os.path.join(EXP_DIR, cfg['experiment_name'], f'run_{run_id:04d}')
+                cfg['run_id'] = run_id
+                config_list.append(cfg)
+                run_id += 1
+    return config_list
